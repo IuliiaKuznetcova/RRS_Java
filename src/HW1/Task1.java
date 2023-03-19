dcd8138c587e374db7c4b34246f48bcd768a5ad9
Iuliia Kuznetcova <iuliia_kuznetcova@hotmail.com>
2023-03-19 11:15:23 +0100

    HW_1

---
+++ src/HW1/Task1.java
@@ -0,0 +1,32 @@
+package HW1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Go!");
+
+        System.out.println();
+        System.out.println("__________________");
+        System.out.println();
+
+        System.out.println("3\n" +
+                "2\n" +
+                "1\n" +
+                "Поехали!!!\n");
+
+        System.out.println();
+        System.out.println("__________________");
+        System.out.println();
+
+        int a = 3;
+        int b = 2;
+        int c = 1;
+        String go = "Go!";
+        String lt = " \n";
+        System.out.println(a+lt+b+lt+c+lt+go);
+
+    }
+
+}
