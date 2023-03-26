fb2538a31c831c5927a2bbdf49e2912f98248395
Iuliia Kuznetcova <iuliia_kuznetcova@hotmail.com>
2023-03-26 17:21:39 +0200

    HW_5 corrected

--- src/HW5/Main.java
+++ src/HW5/Main.java
@@ -46,10 +46,10 @@ public class Main {
             for (int j = 10; j > i; j--) {
                 System.out.print("  ");
             }
-            for (int j = i - 1; j >= 0; j--) {
+            for (int j = i - 1; j > 0; j--) {
                 System.out.print(j + " ");
             }
-            for (int j = i - 1; j >= 0; j--) {
+            for (int j = 0; j <i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
